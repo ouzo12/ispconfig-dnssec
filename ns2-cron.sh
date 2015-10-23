768f9d9bd075318311df56503c5a2f2bb859f8ba
Thomas Adams <ouzo12@gmail.com>
2015-10-23 13:42:10 +0200

    ns2 allow adding transfer

--- ns2-cron.sh
+++ ns2-cron.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 bindpath=/etc/bind
 
+#changeallowtransfer set to 1 will rewrite all zone not allowd (none) into transferallow ips
+changeallowtransfer="1"
+#insert ns2 and any else you want. seperate with ; and end line with ;
+transferallow="1.2.3.4;5.6.7.8;"
+namedconf="$bindpath/named.conf.local"
+
+if [ "$changeallowtransfer" = "1" ]; then
+  sed -i "s/allow-transfer {none;};/allow-transfer {$transferallow};/g" $namedconf
+fi
+
 chown bind:bind $bindpath/pri.*
